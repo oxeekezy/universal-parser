14ed7853f45fca303a60626ae13558d886725e37
oxeekezy <develsecta@hha.com>
2023-02-18 21:54:47 +0300

    Фикс вывода в консоль

--- oxeekparser/cmd_integration.py
+++ oxeekparser/cmd_integration.py
@@ -1,16 +1,27 @@
+import os
+
 from parser import Parser
 import json
 import sys
 
-chrome_path = sys.argv[1]
-profile_path = sys.argv[2]
-sleep = int(sys.argv[3])
-url = sys.argv[4]
+try:
+    chrome_path = sys.argv[1]
+    profile_path = sys.argv[2]
+    sleep = int(sys.argv[3])
+    url = sys.argv[4]
 
-parser = Parser(chrome_path, profile_path, sleep=sleep)
-html = parser.handle(url).get_html()
-response_code = parser.get_response()
+    #TODO: Добавить автоактивацию венва
 
-result = {'html': html, 'response_code': response_code}
+    parser = Parser(chrome_path, profile_path, sleep=sleep)
+    html = parser.handle(url).get_html()
+    response_code = parser.get_response()
 
-print(json.dumps(result))
+    result = {'html': html, 'response_code': response_code}
+except:
+    result = {}
+
+os.system('cls||clear')
+print(result)
+
+
+
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt', 'r') as file:
 
 setuptools.setup(
     name="oxeekparser",
-    version="3.2.0",
+    version="3.3.0",
     author="oxeek",
     author_email="workkukin@gmail.com",
     description="Universal parser with using regex",
